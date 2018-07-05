d414cf7d08a8e5a19181839901e7e60c80d89849
fermi_dirac <ManueleBryan@gmail.com>
2018-07-04 23:12:11 -0700

    added Fermi Dirak Logo

---
+++ README.md
@@ -0,0 +1,3 @@
+#FermiDirak
+
+An open source music visualizer for the web using FFT and the Mozilla Web Audio API
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+import Logo from '../../resources/fermidirak.png';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className='Navbar'>
+        <img className='Navbar-logo' src={Logo}/>
+        <h1>Fermi Dirak</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
